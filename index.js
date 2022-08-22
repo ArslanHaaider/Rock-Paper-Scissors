c9e4ac7d1f89bf0ac5a6cfa6d98d5b119f39e9b4
AslanHaaider <aslanhaider707@gmail.com>
2022-08-22 20:29:43 +0500

    Get move from the Computer

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ let userMove = Number(prompt("Press 0 for Rock / 1 for Paper / 2 Scissors"))
 
 
 // Will asign games Move from gameArray according to user selected number 
-userMove = gameMoves[userMove];
+getuserMove = gameMoves[userMove];
 
+
+// Will get computers Move
+getComputersMove = () => gameMoves[Math.floor(Math.random() * 3)];
 
