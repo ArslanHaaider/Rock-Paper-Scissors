b33fa8e388ea21a23efbfbb888adbd69fde0eff0
AslanHaaider <aslanhaider707@gmail.com>
2022-08-22 18:35:35 +0500

    Ask the user to take move

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rock-Paper-Scissors
+A basic Rock Paper Scissors Game to Implement concepts of Js like loops
+variables functions and conditionals  etc
+Initially it will be just Java script .
+Will lately add CSS styling after gettiing hang of DOM manipulation.
---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Created an Array of all the possible moves a user or Pc can make at their respective turns 
+
+let gameMoves = ["Rock" , "Paper" , "Sicssors"]  
+
+
+// Have to ask user to chose there move
+
+let userMove = Number(prompt("Press 1 for Rock / 2 for Paper / 3 Scissors"))
+
+
