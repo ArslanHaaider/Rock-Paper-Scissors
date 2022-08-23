05b43c5bb4f83633a14e3923f406fe21b8b899e6
AslanHaaider <aslanhaider707@gmail.com>
2022-08-23 13:14:30 +0500

    Create conditions for a win or a Loss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // Created an Array of all the possible moves a user or Pc can make at their respective turns 
 
-let gameMoves = ["Rock" , "Paper" , "Sicssors"]  
+let gameMoves = ["Rock" , "Paper" , "Sicssor"]  
 
 
 // Have to ask user to chose there move
@@ -9,11 +9,15 @@ let gameMoves = ["Rock" , "Paper" , "Sicssors"]
 let userMove ;
 
 // Will asign games Move from gameArray according to user selected number 
-getuserMove = () => gameMoves[ Number(prompt("Press 0 for Rock / 1 for Paper / 2 Scissors"))
-];
 
-
+getuserMove = () => gameMoves[Number(prompt("Press 0 for Rock / 1 for Paper / 2 Scissors"))];
 // Will get computers Move
-getComputersMove = () => gameMoves[Math.floor(Math.random() * 3)];
 
+getComputersMove = () => gameMoves[Math.floor(Math.random() * 3)];
 //Defining Conditions of win and loss! 
+
+if (getuserMove === "Rock" & getComputersMove === "Scissor" || getuserMove === "Paper" & getComputersMove === "Rock" || getuserMove === "Scissor" & getComputersMove =="Paper"){
+    console.log("User wins")
+}else{
+    console.log("Computer wins")    
+}
