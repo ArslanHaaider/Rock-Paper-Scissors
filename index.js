47136f727e8efbbd7e96617bb6f1883229617855
AslanHaaider <aslanhaider707@gmail.com>
2022-08-23 13:28:18 +0500

    Add condition for the draw

--- index.js
+++ index.js
@@ -4,20 +4,30 @@
 let gameMoves = ["Rock" , "Paper" , "Sicssor"]  
 
 
-// Have to ask user to chose there move
-
 let userMove ;
+
 
 // Will asign games Move from gameArray according to user selected number 
 
 getuserMove = () => gameMoves[Number(prompt("Press 0 for Rock / 1 for Paper / 2 Scissors"))];
+
+
+
+
 // Will get computers Move
 
 getComputersMove = () => gameMoves[Math.floor(Math.random() * 3)];
+
+
+
 //Defining Conditions of win and loss! 
 
 if (getuserMove === "Rock" & getComputersMove === "Scissor" || getuserMove === "Paper" & getComputersMove === "Rock" || getuserMove === "Scissor" & getComputersMove =="Paper"){
     console.log("User wins")
+}else if (getuserMove === getComputersMove){
+    console.log ("Its Draw")
 }else{
     console.log("Computer wins")    
 }
+
+
